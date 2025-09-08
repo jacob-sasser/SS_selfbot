deee961792aae2317eb8ef8c34340c3bbc9e389f
jacob-sasser <jacobsasser23@gmail.com>
2025-09-08 11:47:06 -0400

    added get_active_channels

--- head_bot.py
+++ head_bot.py
@@ -3,6 +3,8 @@ import asyncio
 from discord.ext import commands,tasks
 from typing import Optional
 
+TOKEN=''
+
 class main_bot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -13,25 +15,59 @@ class main_bot(commands.Cog):
         return role.members
 
 
-    @tasks.loop(seconds=10)
-    async def get_inactive_bots(self,ctx,channel_list:list[discord.VoiceChannel],bots:list[discord.Member]):
+    @tasks.loop(seconds=5)
+    async def get_inactive_bots(self,ctx,afk_channel:discord.VoiceChannel):
         '''
         Gets the current bots not watching a screenshare
         params: 
-        channel_list: A list of discord.VoiceChannels that will be searched through
+        afk_channel: the waiting channel for the bots
         bots: a list of discord.Members that have a bot role
         returns: list[discord.Member]
         '''
-        inactive_bots=[]
-        for bot in bots:
-            if not bot.voice or bot.voice.channel not in channel_list:
-                inactive_bots.append(bot)
-        return inactive_bots
+        bots_in_channel=afk_channel.members
+        return bots_in_channel
 
+    @tasks.loop(seconds=1)
+    async def get_active_channels(self,ctx,channel_list:list[discord.VoiceChannel],bot_role:discord.Role,human_role:discord.Role):
+        '''
+        searches through the designated list of channels and returns the channels with a (non-bot) user and a bot-user in them
+        
+        '''
+        active_channel=[]
+        
+        for channel in channel_list:
+            has_bot=False
+            has_member=False
+            members=channel.members
+            for member in members:
+                if bot_role in member.roles:
+                    has_bot=True
+                elif human_role in member.roles:
+                    has_member=True
+            if has_member == True & has_bot==True:
+                active_channel.append(channel)
+            
+        return active_channel
+
+        
+        
 
     @commands.command()
-    async def init_bot(self,ctx,bot:discord.Member):
-        pass
+    async def init_bot(self,ctx,bot:discord.Member,bot_role:discord.Role):
+        '''
+        initialized a new slave bot, giving them roles and server muting & deafning them
+        potentially also gives them nickname IDK yet
+        
+        returns: void
+        '''
+        if(bot):
+            bot.edit(role=bot_role,mute=True,deafen=True)
+            ctx.send(f"Initialized {bot}")
+            
+        else:
+            ctx.send("User does not exist")
+        
+
 
     @commands.Cog.listener()
     def move_bot(self,ctx,bot:discord.Member,*,channel:discord.VoiceChannel):
